d9af327dd89e07f9c8bdced0fe2f58d6743ead56
Carly Williams <carly.kurth@gmail.com>
2016-05-10 21:28:16 -0700

    Initial commit

---
+++ apps/app.js
@@ -0,0 +1,36 @@
+$(function(){
+
+	$("#btn-submit").click(function(event){
+		event.preventDefault();
+		$("#search-results").html('');
+		var query = $("#query").val();
+		getRequest(query);
+
+	});
+
+	function getRequest(searchTerm) {
+		
+		var params = {
+			s: searchTerm,
+			r: 'json'
+		};
+
+		url = 'http://www.omdbapi.com';
+
+		$.getJSON(url, params, function(data){
+			showResults(data.Search);
+  	});		
+	}
+
+  function showResults(results) {
+  	/*
+		for (var i = 0; i < myData.length; i++) {
+			console.log(myData[i].Title);
+		}
+    */
+  	$.each(results, function(index, value){
+  		$("#search-results").append('<p>' + value.Title + '</p>');
+      console.log(value.Title);
+    });
+  }
+});
